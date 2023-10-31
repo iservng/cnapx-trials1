b6e4367dac46665ae81f147a3437815f914cc22b
iservng <iservng@gmail.com>
2023-10-31 13:35:11 +0100

    Just hosted the app in the cloud

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyCrfKDEJg8Y9Xn4B2yAWdbuMoQUvP_qk5U",
+    authDomain: "cnapx-5a1c0.firebaseapp.com",
+    projectId: "cnapx-5a1c0",
+    storageBucket: "cnapx-5a1c0.appspot.com",
+    messagingSenderId: "358469353004",
+    appId: "1:358469353004:web:33b7cfc71ce29c09d89b89"
+  };
+initializeApp(firebaseConfig);
+
